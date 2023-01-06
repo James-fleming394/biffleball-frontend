38b586aa32d4431fd8f722b77806b3285bd07d55
james-fleming394 <james.fleming394@gmail.com>
2023-01-06 12:47:50 -0500

    Navbar auth implement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { CheckSession } from './services/auth.js';
+// import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const handleLogout = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  };
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar
+        authenticated={authenticated}
+        user={user}
+        handleLogout={handleLogout}
+      />
+      
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ authenticated, user, handleLogout }) => {
+    let authenticatedOptions
+    if (user) {
+        authenticatedOptions = (
+            <nav>
+                <Link className="link" to="/picks">Picks</Link>
+                <Link className="link" to="/standings">Standings</Link>
+                <Link className="link" to="/profile">Profile</Link>
+                <Link className="link" onClick={handleLogout} to="/">Sign Out</Link>
+            </nav>
+        )
+    }
+
+    const publicOptions = (
+        <nav>
+            <Link className="link" to="/">Home</Link>
+            <Link className="link" to="/register">Register</Link>
+            <Link className="link" to="/signin">Sign In</Link>
+        </nav>
+    )
+
+    return (
+        <div>
+            {authenticated && user ? authenticatedOptions : publicOptions}
+        </div>
+    )
 
 }
 
