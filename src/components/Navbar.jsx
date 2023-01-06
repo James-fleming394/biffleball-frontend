7984a0a45ee7797fdc65ea3260526a94efda2b2e
james-fleming394 <james.fleming394@gmail.com>
2023-01-06 11:25:26 -0500

    Navbar links create

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    
+    return (
+        <div className="navbar">
+            <nav className="nav">
+                <h2 className="logo">BiffleBall Logo Here</h2>
+                <Link className="nav-link"to="/">Home</Link>
+                <Link className="nav-link"to="/picks">Picks</Link>
+                <Link className="nav-link"to="/standings">Standings</Link>
+                <Link className="nav-link"to="/profile">Profile</Link>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar;
