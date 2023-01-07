09c7622d4dc2640673d504fe1266e14240e7328e
james-fleming394 <james.fleming394@gmail.com>
2023-01-06 19:27:26 -0500

    Nav update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({ authenticated, user, handleLogout }) => {
-    let authenticatedOptions
-    if (user) {
-        authenticatedOptions = (
-            <nav>
-                <Link className="link" to="/picks">Picks</Link>
-                <Link className="link" to="/standings">Standings</Link>
-                <Link className="link" to="/profile">Profile</Link>
-                <Link className="link" onClick={handleLogout} to="/">Sign Out</Link>
-            </nav>
-        )
-    }
+const Navbar = () => {
+    // let authenticatedOptions
+    // if (user) {
+    //     authenticatedOptions = (
+    //         <nav>
+    //             <Link className="link" to="/standings">Standings</Link>
+    //             <Link className="link" to="/submit-pick">Submit Pick</Link>
+    //             <Link className="link" to="/stats">Advanced Stats</Link>
+    //             <Link className="link" to="/profile">Profile</Link>
+    //             <Link className="link" onClick={handleLogout} to="/">Sign Out</Link>
+    //         </nav>
+    //     )
+    // }
 
-    const publicOptions = (
-        <nav>
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="/register">Register</Link>
-            <Link className="link" to="/signin">Sign In</Link>
-        </nav>
-    )
+    // const publicOptions = (
+    //     <nav>
+    //         <Link className="link" to="/standings">Standings</Link>
+    //         <Link className="link" to="/submit-pick">Submit Pick</Link>
+    //         <Link className="link" to="/stats">Advanced Stats</Link>
+    //         <Link className="right-link" to="/register">Register</Link>
+    //         <Link className="right-link" to="/signin">Login</Link>
+    //     </nav>
+    // )
 
     return (
         <div>
             <header>
             <Link to="/">
                 <div className="logo">
-                    <img className="header-logo"
-                    src="/img/Biffle-Small-White.png"
-                    alt="welcome logo"
-                    />
+                    <img className="header-logo" src="/img/Biffle-Small-White.png" alt="welcome logo"/>
+                    <nav1>
+                        <ul className="links">
+                            <li><Link className="link" to="/standings">Standings</Link></li>
+                            <li><Link className="link" to="/stats">Stats</Link></li>
+                            <li><Link className="link" to="/submit-pick">Submit a Pick</Link></li>
+                            <li><Link className="link" to="/rules">Rules</Link></li>
+                        </ul>
+                    </nav1>
+                    <nav2>
+                        <ul className="links2">
+                        <li><Link className="link" to="/login">Login</Link></li>
+                        </ul>
+                    </nav2>
                 </div>
             </Link>
-            {authenticated && user ? authenticatedOptions : publicOptions}
-        </header>
+            </header>
         </div>
     )
 
