235ad3893a06cd1b22f9f1c8c15ac754b7a71605
james-fleming394 <james.fleming394@gmail.com>
2023-01-11 09:41:56 -0500

    Bot-nav set up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,14 +26,34 @@ const Navbar = () => {
     // )
 
     return (
+        <>
         <div className="top-bar">
-            <span><img className='BB-logo' src="/img/b-solid.png" /><img className='BB-logo2' src="/img/b-solid.png" /></span>
+            <span><img className='BB-logo' src="/img/b-solid.png" alt="logo" /><img className='BB-logo2' src="/img/b-solid.png" alt="logo" /></span>
             <ul>
-                <li><a href="https://twitter.com/Biffle_Ball"><img src="/img/twitter.png"/></a></li>
-                <li><a href="https://discord.gg/Vwz5yffp"><img src="/img/discord.png"/></a></li>
-                <li><a href="mailto:register@biffleball.com"><img src="/img/email.png"/></a></li>
+                <li><a href="https://twitter.com/Biffle_Ball"><img src="/img/twitter.png" alt="twitter"/></a></li>
+                <li><a href="https://discord.gg/Vwz5yffp"><img src="/img/discord.png" alt="discord" /></a></li>
+                <li><a href="mailto:register@biffleball.com"><img src="/img/email.png" alt="email" /></a></li>
+                <Link>Register</Link>
+                <Link>Login</Link>
             </ul>
         </div>
+        <nav>
+            <div className="logo">
+                <Link to='/'>
+                <img src="/img/Biffle-small-black.png" alt="logo" />
+                </Link>
+            </div>
+            <div className="toggle">
+                <a href=""><img src="/img/bars-solid.png" alt="menu"/></a>
+            </div>
+            <ul className="menu">
+                <li><Link to="/standings">Standings</Link></li>
+                <li><Link to="/stats">Stats</Link></li>
+                <li><Link to="/submit-pick">Submit Pick</Link></li>
+                <li><Link to="/rules">Rules</Link></li>
+            </ul>
+        </nav>
+            </>
     )
 
 }
