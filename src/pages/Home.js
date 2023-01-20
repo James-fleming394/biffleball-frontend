719a7f07f9735114a1cec1990e760d02b5a6549f
james-fleming394 <james.fleming394@gmail.com>
2023-01-20 11:15:29 -0500

    Get started button complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 import TwitterContainer from "../components/TwitterContainer";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+    let navigate = useNavigate();
+
+    const registerClick = () => {
+        navigate("/register")
+    }
+
     return (
         <div className="home">
             <div className="logo-home">
@@ -9,7 +17,9 @@ const Home = () => {
             </div>
             <div className="welcome">
                 <h1>Modernizing America's Pastime</h1>
-                <button>Get Started</button>
+                <div className="span">
+                    <a onClick={registerClick}></a>
+                </div>
             </div>
             <div className="twitter-widget">
                 <TwitterContainer />
