457942d350addaea79a3a592c3aefe3e7cf52ecd
james-fleming394 <james.fleming394@gmail.com>
2023-01-23 10:22:07 -0500

    Tabs set up

--- src/components/HomeTabs.jsx
+++ src/components/HomeTabs.jsx
@@ -6,10 +6,26 @@ const Tabs = () => {
     const toggleTab = (index) => {
         setToggleState(index)
     };
-    
+
     return (
         <div className="Tabs">
-            
+            <div className="block-tabs">
+                <button className={toggleState === 1 ? "tabs active tabs" : "tabs"}
+                        onClick={() => toggleTab(1)}
+                >
+                About
+                </button>
+                <button className={toggleState === 2 ? "tabs active tabs" : "tabs"}
+                        onClick={() => toggleTab(2)}
+                >
+                How to Play
+                </button>
+                <button className={toggleState === 3 ? "tabs active tabs" : "tabs"}
+                        onClick={() => toggleTab(3)}
+                >
+                Entry + Prizes
+                </button>
+            </div>
         </div>
     )
 }
