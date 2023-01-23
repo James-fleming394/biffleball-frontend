48eab20cd63be1f8c967c39a7df0e5f82c91efac
james-fleming394 <james.fleming394@gmail.com>
2023-01-23 14:46:40 -0500

    Timer working

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Timer = () => {
     const [days, setDays] = useState(0);
@@ -7,28 +7,44 @@ const Timer = () => {
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
 
-    const deadline = "March 30th, 2023";
+    const deadline = "March, 30, 2023";
 
     const getTime = () => {
-        const time = Date.parse(deadline) = Date.now();
+        const time = Date.parse(deadline) - Date.now();
 
-        setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
-        setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
-        setMinutes(Math.floor((time / 1000 / 60) % 60));
-        setSeconds(Math.floor((time / 1000) % 60));
-    }
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+};
 
     useEffect(() => {
-        const interval = setInterval(() => getTime(deadline), 1000);
+    const interval = setInterval(() => getTime(deadline), 1000);
 
-        return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+}, []);
 
     return (
-        <div className="timer">
-            <h1>Countdown to the Season:</h1>
+    <div className="timer-entire">
+        <h1>Countdown to Opening Day:</h1>
+        <div className='timer'>
+            <h2>{days}</h2>
+            <h4>Days</h4>
         </div>
-    )
-}
+        <div className='timer'>
+            <h2>{hours}</h2>
+            <h4>Hours</h4>
+        </div>
+        <div className='timer'>
+            <h2>{minutes}</h2>
+            <h4>Minutes</h4>
+        </div>
+        <div className='timer'>
+            <h2>{seconds}</h2>
+            <h4>Seconds</h4>
+        </div>
+    </div>
+    );
+};
 
 export default Timer;
