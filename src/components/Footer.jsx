424a736354ff1748bbaa0182ec775676c201431a
james-fleming394 <james.fleming394@gmail.com>
2023-01-23 15:53:36 -0500

    Footer CSS complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,14 @@ const Footer = () => {
 
     return (
         <footer>
+            <p>Conditions of Use
+                <br></br>
+                Privacy Notice
+                <br></br>
+                Your Ads Privacy Choices</p>
             <span><img className='footer-img' src="/img/Biffle-Small-White.png" alt="logo" /></span>
-            <p>Conditions of Use        Privacy Notice      Your Ads Privacy Choices</p>
-            <p>© 2020-2023, Snake Pit LLC, Inc. or its affiliates</p>
-            <p>register@biffleball.com</p>
+            <p>© 2020-2023, Snake Pit LLC. or its affiliates</p>
+            
         </footer>
     )
 }
