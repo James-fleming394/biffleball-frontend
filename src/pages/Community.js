7869f7b023b55229c2b703dd6884803494773bb7
james-fleming394 <james.fleming394@gmail.com>
2023-01-29 18:03:26 -0500

    Community page complete and CSS complete

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Community = () => {
     
+    let navigate = useNavigate();
+
+    return (
+        <>
+        <div className="community">
+            <h1>Community</h1>
+        </div>
+        <div className="category">
+            <div className="title-category">
+                <h2 className="communityh2">Announcements</h2>
+            </div>
+            <img className="category-image" 
+            src="https://c1.wallpaperflare.com/preview/501/991/764/baseball-crowd-game-lights.jpg" />
+        </div>
+        <div className="category">
+            <div className="title-category">
+                <h2 className="communityh2">Blog</h2>
+            </div>
+            <img className="category-image" 
+            src="https://c1.wallpaperflare.com/preview/377/306/543/baseball-field-base-ball-scenery-lights.jpg" />
+        </div>
+        <div className="category">
+            <div className="title-category">
+                <h2 className="communityh2">Questions</h2>
+            </div>
+            <img className="category-image" 
+            src="https://img.mlbstatic.com/mlb-photos/image/upload/w_1500,q_100/v1/people/605137/action/hero/current" />
+        </div>
+        </>
+    )
 }
 
 export default Community;
